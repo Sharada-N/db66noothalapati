04b4df228fa0776f3656c7b97d78144ef986fe04
Sharada-N <77762950+Sharada-N@users.noreply.github.com>
2021-04-14 20:40:44 -0500

    added new views and bonus

--- controllers/parking.js
+++ controllers/parking.js
@@ -24,57 +24,122 @@ exports.parking_detail = async function(req, res) {
 
 };
 
-
-// Handle parking delete form on DELETE.
-exports.parking_delete= function(req, res) {
-    res.send('NOT IMPLEMENTED: parking delete DELETE '+ req.params.id);
-};
-
-// Handle parking update form on PUT.
-exports.parking_update_put = async function(req, res) {
-console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
-try {
-    let toUpdate = await parking.findById( req.params.id)
-// Do updates of properties
-    if(req.body.Types) toUpdate.Types = req.body.Types;
-    if(req.body.ticket) toUpdate.ticket = req.body.ticket;
-    if(req.body.capacity) toUpdate.capacity = req.body.capacity;
-    let result = await toUpdate.save();
-    console.log("Sucess " + result)
-    res.send(result)
-    } 
-    catch (err) {
-        res.status(500)
-        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
-    }
-};
-
-exports.parking_view_all_Page= async function(req, res) {
-    try{
-        theparking= await parking.find();
-        res.render('parking', { title:'parking Search Results', results: theparking});
-    }catch(err){
-        res.error(500,`{"error": ${err}}`);
-    }
-};
-
-
 // Handle parking create on POST.
-exports.parking_create_post= async function(req, res) {
+exports.parking_create_post = async function (req, res) {
     console.log(req.body)
-    let document= new parking();
+    let document = new parking();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"parkingtype":"goat", "cost":12, "size":"large"}
-    document.Type = req.body.Type;
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.Types = req.body.Types;
     document.ticket = req.body.ticket;
     document.capacity = req.body.capacity;
-    try{
-        let result= await document.save();
+    try {
+        let result = await document.save();
         res.send(result);
     }
+    catch (err) {
+        res.send(`{"error": ${err}}`)
+        res.status(500);
+    }
+};
+
+// Handle parking delete form on DELETE.
+exports.parking_delete = async function(req, res) {
+    console.log("delete" + req.params.id)
+    try {
+        result = await parking.findByIdAndDelete(req.params.id)
+        console.log("Removed" + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
+//Handle parking update form on PUT.
+exports.parking_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await parking.findById( req.params.id)
+    // Do updates of properties
+        if(req.body.Types) toUpdate.Types = req.body.Types;
+        if(req.body.ticket) toUpdate.ticket = req.body.ticket;
+        if(req.body.capacity) toUpdate.capacity = req.body.capacity;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } 
+    catch (err) {
+            res.status(500)
+            res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+};
+    
+// VIEWS
+// Handle a show all view
+exports.parking_view_all_Page = async function (req, res) {
+    try {
+        theparkings = await parking.find();
+        res.render('parkings', { title: 'parking Search Results', results: theparkings });
+    }
+    catch (err) {
+        res.send(`{"error": ${err}}`)
+        res.status(500);
+    }
+};
+    
+// Handle a show one view with id specified by query
+exports.parking_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await parking.findById( req.query.id)
+        res.render('parkingdetail', { title: 'parking Detail', toShow: result });
+    }
     catch(err){
-        res.error(500,`{"error": ${err}}`);
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+    
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.parking_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+        res.render('parkingcreate', { title: 'parking Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+    
+// Handle building the view for updating a fish.
+// query provides the id
+exports.parking_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await parking.findById(req.query.id)
+        res.render('parkingupdate', { title: 'parking Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+    
+// Handle a delete one view with id from query
+exports.parking_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await parking.findById(req.query.id)
+        res.render('parkingdelete', { title: 'parking Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
     }
 };
--- routes/parking.js
+++ routes/parking.js
@@ -5,3 +5,15 @@ var router= express.Router();
 /* GET parkings */
 router.get('/',parking_controlers.parking_view_all_Page);
 module.exports= router;
+
+/* GET detail parking page */
+router.get('/detail', parking_controlers.parking_view_one_Page);
+
+/* GET create parking page */
+router.get('/create', parking_controlers.parking_create_Page);
+
+/* GET create update page */
+router.get('/update', parking_controlers.parking_update_Page);
+
+/* GET create parking page */
+router.get('/delete', parking_controlers.parking_delete_Page);
